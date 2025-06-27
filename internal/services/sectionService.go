127dbbf1d0cfcbf23943c40a509f76ad5b260742
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-06-27 16:02:09 -0300

    adding method that return all sections

--- internal/services/sectionService.go
+++ internal/services/sectionService.go
@@ -1,9 +1,24 @@
 package services
 
+import (
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/repositories"
+)
+
 func GetSectionService() SectionServiceI {
-	return &SectionService{}
+	return &sectionService{
+		repository: repositories.GetSectionRepository(),
+	}
 }
 
-type SectionServiceI interface{}
+type SectionServiceI interface {
+	GetAll() []*models.Section
+}
 
-type SectionService struct{}
+type sectionService struct {
+	repository repositories.SectionRepositoryI
+}
+
+func (s *sectionService) GetAll() []*models.Section {
+	return s.repository.GetAll()
+}
