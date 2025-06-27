f24aea4a5c3b79a091befa25bcaa597a0ab00974
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-06-27 16:01:21 -0300

    adding methods that map the section model to section response

--- internal/mappers/sectionMapper.go
+++ internal/mappers/sectionMapper.go
@@ -1,1 +1,30 @@
 package mappers
+
+import (
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/responses"
+)
+
+func GetSectionResponseFromModel(model *models.Section) *responses.SectionResponse {
+	return &responses.SectionResponse{
+		ID:                 model.ID,
+		SectionNumber:      model.SectionNumber,
+		CurrentCapacity:    model.CurrentCapacity,
+		CurrentTemperature: model.CurrentTemperature,
+		MaximumCapacity:    model.MaximumCapacity,
+		MinimumCapacity:    model.MinimumCapacity,
+		MinimumTemperature: model.MinimumTemperature,
+		ProductTypeID:      model.ProductTypeID,
+		WarehouseID:        model.WarehouseID,
+	}
+}
+
+func GetListSectionResponseFromListModel(models []*models.Section) []*responses.SectionResponse {
+	var listSectionResponse []*responses.SectionResponse
+	if len(models) > 0 {
+		for _, s := range models {
+			listSectionResponse = append(listSectionResponse, GetSectionResponseFromModel(s))
+		}
+	}
+	return listSectionResponse
+}
