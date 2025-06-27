1ddf25642f568b6ad8b428453899dca8e9e445c0
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-06-27 16:04:18 -0300

    adding file sectionHnadler.go with the method that returns the response for all sections

---
+++ internal/handlers/sectionHandler.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/mappers"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/responses"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/services"
+)
+
+func GetSectionHandler() SectionHandlerI {
+	return &SectionHandler{
+		service: services.GetSectionService(),
+	}
+}
+
+type SectionHandlerI interface{}
+
+type SectionHandler struct {
+	service services.SectionServiceI
+}
+
+func (h *SectionHandler) GetAll() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			response         *responses.DataResponse = &responses.DataResponse{}
+			sectionsResponse []*responses.SectionResponse
+			sections         []*models.Section
+		)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		sections = h.service.GetAll()
+		sectionsResponse = mappers.GetListSectionResponseFromListModel(sections)
+		response.Data = sectionsResponse
+
+		w.WriteHeader(http.StatusOK)
+		encodeErr := json.NewEncoder(w).Encode(response)
+		if encodeErr != nil {
+			response.SetError(encodeErr.Error())
+			w.WriteHeader(http.StatusExpectationFailed)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+}
