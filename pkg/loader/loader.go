0bb59b23d9a1ec3e7619f52701e369a28f2c8149
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-06-27 18:47:44 -0300

    updating the MapToSlice method to return a slice of memory directions

--- pkg/loader/loader.go
+++ pkg/loader/loader.go
@@ -38,6 +38,7 @@ func (s *StorageJSON[T]) ReadAll() (map[int]T, error) {
 
 	var list []T
 	decoder := json.NewDecoder(file)
+
 	if err := decoder.Decode(&list); err != nil {
 		return nil, fmt.Errorf("error decodificando JSON: %w", err)
 	}
@@ -83,13 +84,13 @@ func (s *StorageJSON[T]) WriteAll(items []T) error {
 // MapToSlice converts a generic map[int]T to a generic slice []T.
 // It iterates through the values of the input map and appends them
 // to a new slice, which is then returned.
-func (s *StorageJSON[T]) MapToSlice(items map[int]T) []T {
+func (s *StorageJSON[T]) MapToSlice(items map[int]T) []*T {
 
-	var itemsSlice = make([]T, 0)
+	var itemsSlice = make([]*T, 0)
 
 	for _, value := range items {
 		// Append each value from the map to the itemsSlice.
-		itemsSlice = append(itemsSlice, value)
+		itemsSlice = append(itemsSlice, &value)
 	}
 
 	// Return the newly created slice containing all the values from the map.
