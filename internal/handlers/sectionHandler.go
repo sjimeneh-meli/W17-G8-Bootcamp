a03617e13d49572448b079151dcbf2e6813fea44
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-06-30 13:36:02 -0300

    adding endpoint for getting the section by its id

--- internal/handlers/sectionHandler.go
+++ internal/handlers/sectionHandler.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/error_message"
 	"github.com/sajimenezher_meli/meli-frescos-8/internal/handlers/responses"
 	"github.com/sajimenezher_meli/meli-frescos-8/internal/mappers"
 	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
@@ -18,6 +21,7 @@ func GetSectionHandler() SectionHandlerI {
 
 type SectionHandlerI interface {
 	GetAll() http.HandlerFunc
+	GetByID() http.HandlerFunc
 }
 
 type SectionHandler struct {
@@ -37,6 +41,45 @@ func (h *SectionHandler) GetAll() http.HandlerFunc {
 		sections = h.service.GetAll()
 		sectionsResponse = mappers.GetListSectionResponseFromListModel(sections)
 		response.Data = sectionsResponse
+
+		w.WriteHeader(http.StatusOK)
+		encodeErr := json.NewEncoder(w).Encode(response)
+		if encodeErr != nil {
+			response.SetError(encodeErr.Error())
+			w.WriteHeader(http.StatusExpectationFailed)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+}
+
+func (h *SectionHandler) GetByID() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			response        *responses.DataResponse = &responses.DataResponse{}
+			sectionResponse *responses.SectionResponse
+		)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		idParam, convErr := strconv.Atoi(chi.URLParam(r, "id"))
+		if convErr != nil {
+			response.SetError(error_message.ErrInvalidInput.Error())
+			w.WriteHeader(http.StatusExpectationFailed)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		section, srvErr := h.service.GetByID(idParam)
+		if srvErr != nil {
+			response.SetError(srvErr.Error())
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		sectionResponse = mappers.GetSectionResponseFromModel(section)
+		response.Data = sectionResponse
 
 		w.WriteHeader(http.StatusOK)
 		encodeErr := json.NewEncoder(w).Encode(response)
