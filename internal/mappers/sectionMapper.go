b22a8fb8708bee0652ff936abbe099d1e28612de
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-07-01 10:18:04 -0300

    adding method that map the request with the model

--- internal/mappers/sectionMapper.go
+++ internal/mappers/sectionMapper.go
@@ -1,9 +1,23 @@
 package mappers
 
 import (
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/handlers/requests"
 	"github.com/sajimenezher_meli/meli-frescos-8/internal/handlers/responses"
 	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
 )
+
+func GetSectionModelFromRequest(request *requests.SectionRequest) *models.Section {
+	return &models.Section{
+		SectionNumber:      request.SectionNumber,
+		CurrentCapacity:    request.CurrentCapacity,
+		CurrentTemperature: request.CurrentTemperature,
+		MaximumCapacity:    request.MaximumCapacity,
+		MinimumCapacity:    request.MinimumCapacity,
+		MinimumTemperature: request.MinimumTemperature,
+		ProductTypeID:      request.ProductTypeID,
+		WarehouseID:        request.WarehouseID,
+	}
+}
 
 func GetSectionResponseFromModel(model *models.Section) *responses.SectionResponse {
 	return &responses.SectionResponse{
