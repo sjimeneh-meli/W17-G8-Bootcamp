70fcad6131d2525c3bc32124a233c488db62cc5d
galopezperez_meli <gabriel.lopezperez@mercadolibre.com>
2025-07-01 11:50:31 -0300

    adding endpoint that updates a section

--- internal/handlers/sectionHandler.go
+++ internal/handlers/sectionHandler.go
@@ -26,6 +26,7 @@ type SectionHandlerI interface {
 	GetAll() http.HandlerFunc
 	GetByID() http.HandlerFunc
 	Create() http.HandlerFunc
+	Update() http.HandlerFunc
 	DeleteByID() http.HandlerFunc
 }
 
@@ -149,6 +150,66 @@ func (h *SectionHandler) Create() http.HandlerFunc {
 			return
 		}
 
+	}
+}
+
+func (h *SectionHandler) Update() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			request  *requests.SectionRequest = &requests.SectionRequest{}
+			response *responses.DataResponse  = &responses.DataResponse{}
+		)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		idParam, convErr := strconv.Atoi(chi.URLParam(r, "id"))
+		if convErr != nil {
+			response.SetError(error_message.ErrInvalidInput.Error())
+			w.WriteHeader(http.StatusExpectationFailed)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		section, srvErr := h.service.GetByID(idParam)
+		if srvErr != nil {
+			response.SetError(srvErr.Error())
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		if reqErr := json.NewDecoder(r.Body).Decode(request); reqErr != nil {
+			response.SetError(error_message.ErrInvalidInput.Error())
+			w.WriteHeader(http.StatusExpectationFailed)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		if valErr := h.validation.ValidateSectionRequestStruct(*request); valErr != nil {
+			response.SetError(valErr.Error())
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		if h.service.ExistsWithSectionNumber(request.SectionNumber) {
+			response.SetError("already exist a section with the same number")
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		mappers.UpdateSectionModelFromRequest(section, request)
+
+		response.Data = mappers.GetSectionResponseFromModel(section)
+		w.WriteHeader(http.StatusCreated)
+		encodeErr := json.NewEncoder(w).Encode(response)
+		if encodeErr != nil {
+			w.WriteHeader(http.StatusExpectationFailed)
+			response.SetError(encodeErr.Error())
+			json.NewEncoder(w).Encode(response)
+			return
+		}
 	}
 }
 
