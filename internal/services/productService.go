5575dd37d1957014a4b0029cb7ef52d0a63690e3
sajimenezher_meli <samuel.jimenezhernandez@mercadolibre.com.co>
2025-06-30 21:59:46 -0500

    Added service logic to the product

--- internal/services/productService.go
+++ internal/services/productService.go
@@ -1,1 +1,49 @@
 package services
+
+import (
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
+)
+
+type ProductServiceI interface {
+	GetAll() ([]models.Product, error)
+	GetByID(id int) (*models.Product, error)
+	Create(product models.Product) (models.Product, error)
+	UpdateById(id int, product models.Product) (models.Product, error)
+	DeleteById(id int) error
+	ExistById(id int) bool
+}
+
+type productService struct {
+	repository ProductServiceI
+}
+
+func NewProductService(repository ProductServiceI) ProductServiceI {
+	return &productService{
+		repository: repository,
+	}
+}
+
+func (ps *productService) GetAll() ([]models.Product, error) {
+	return ps.repository.GetAll()
+}
+
+func (ps *productService) GetByID(id int) (*models.Product, error) {
+	return ps.repository.GetByID(id)
+}
+
+func (ps *productService) Create(newProduct models.Product) (models.Product, error) {
+	return ps.repository.Create(newProduct)
+}
+
+func (ps *productService) UpdateById(id int, updateProduct models.Product) (models.Product, error) {
+	return ps.repository.UpdateById(id, updateProduct)
+}
+
+func (ps *productService) DeleteById(id int) error {
+	return ps.repository.DeleteById(id)
+}
+
+func (ps *productService) ExistById(id int) bool {
+	return ps.ExistById(id)
+
+}
