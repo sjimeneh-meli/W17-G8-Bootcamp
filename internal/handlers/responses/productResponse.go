36cc935af0e80288e931daddf2d95afe82c6a5f7
sajimenezher_meli <samuel.jimenezhernandez@mercadolibre.com.co>
2025-06-30 20:46:56 -0500

    Added productResponse struct in responses

--- internal/handlers/responses/productResponse.go
+++ internal/handlers/responses/productResponse.go
@@ -1,1 +1,16 @@
 package responses
+
+type Product struct {
+	Id                             string  `json:"id"`
+	ProductCode                    string  `json:"product_code"`
+	Description                    string  `json:"description"`
+	Width                          float64 `json:"width"`
+	Height                         float64 `json:"height"`
+	Length                         float64 `json:"length"`
+	NetWeight                      float64 `json:"net_weight"`
+	ExpirationRate                 float64 `json:"expiration_rate"`
+	RecommendedFreezingTemperature float64 `json:"recommended_freezing_temperature"`
+	FreezingRate                   float64 `json:"freezing_rate"`
+	ProductTypeID                  int     `json:"product_type_id"`
+	SellerID                       *int    `json:"seller_id,omitempty"` // Usamos un puntero para indicar que no es obligatorio
+}
