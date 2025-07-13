4e67d5675904774170a4dc2a20bce354d070e0e6
Galp343 <gabriel.lopezperez@mercadolibre.com>
2025-07-13 13:41:05 -0300

    new model product batch

---
+++ internal/models/productBatch.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type ProductBatch struct {
+	Id                 int       `json:"id"`
+	BatchNumber        string    `json:"batch_number"`
+	CurrentQuantity    int       `json:"current_quantity"`
+	CurrentTemperature float64   `json:"current_temperature"`
+	DueDate            time.Time `json:"due_date"`
+	InitialQuantity    int       `json:"initial_quantity"`
+	ManufacturingDate  time.Time `json:"manufacturing_date"`
+	ManufacturingHour  time.Time `json:"manufacturing_hour"`
+	MinimumTeperature  float64   `json:"minimum_temperature"`
+	ProductID          int       `json:"product_id"`
+	SectionID          int       `json:"section_id"`
+}
