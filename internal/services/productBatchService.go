7486e8dc85a36efded4d608409f627755b16e11f
Galp343 <gabriel.lopezperez@mercadolibre.com>
2025-07-15 11:35:37 -0300

    adding method to create and asks if exists another with the same number

--- internal/services/productBatchService.go
+++ internal/services/productBatchService.go
@@ -1,1 +1,29 @@
 package services
+
+import (
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/models"
+	"github.com/sajimenezher_meli/meli-frescos-8/internal/repositories"
+)
+
+func GetProductBatchService(repository repositories.ProductBatchRepositoryI) ProductBatchServiceI {
+	return &productBatchService{
+		repository: repository,
+	}
+}
+
+type ProductBatchServiceI interface {
+	Create(model *models.ProductBatch) error
+	ExistsWithBatchNumber(id int, batchNumber string) bool
+}
+
+type productBatchService struct {
+	repository repositories.ProductBatchRepositoryI
+}
+
+func (s *productBatchService) Create(model *models.ProductBatch) error {
+	return s.repository.Create(model)
+}
+
+func (s *productBatchService) ExistsWithBatchNumber(id int, batchNumber string) bool {
+	return s.repository.ExistsWithBatchNumber(id, batchNumber)
+}
